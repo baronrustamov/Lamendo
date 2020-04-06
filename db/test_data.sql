d95864bee5f9a54cdd09e7695368fbe82c18b824
unknown <mp.hladun@gmail.com>
2020-04-06 17:05:38 -0400

    feat: image uploads, css, etc.

--- db/test_data.sql
+++ db/test_data.sql
@@ -7,28 +7,32 @@ insert into board(board_acronym, board_description) values ('g', 'Technology');-
 insert into board(board_acronym, board_description) values ('ck', 'Food & Cooking');--board_id=2
 insert into board(board_acronym, board_description) values ('b', 'Random');--board_id=3
 
+
 -- create table post (
 --     post_id integer primary key,
 --     post_board_id integer,
 --     user text not null,
 --     date text not null,
---     post text not null,
---     image_file text,
+--     post text,
+--     img_filename text,
+--     img_uniqid text,
 --     foreign key(post_board_id) references board(board_id)
 -- );
-insert into post(post_board_id, user, date, post, image_file)
-    values (1, 'anon11111', strftime('%Y-%m-%d %H:%M', 'now', 'localtime'), 'roll', null);
-insert into post(post_board_id, user, date, post, image_file)
-    values (2, 'anon22222', strftime('%Y-%m-%d %H:%M', 'now', 'localtime'), 'roll', null);
-insert into post(post_board_id, user, date, post, image_file)
-    values (3, 'anon33333', strftime('%Y-%m-%d %H:%M', 'now', 'localtime'), 'roll', null);
+insert into post(post_board_id, user, date, post, image_filename, image_uniqid)
+    values (1, 'anon11111', strftime('%Y-%m-%d %H:%M', 'now', 'localtime'), 'roll', null, null);
+insert into post(post_board_id, user, date, post, image_filename, image_uniqid)
+    values (2, 'anon22222', strftime('%Y-%m-%d %H:%M', 'now', 'localtime'), 'roll', null, null);
+insert into post(post_board_id, user, date, post, image_filename, image_uniqid)
+    values (3, 'anon33333', strftime('%Y-%m-%d %H:%M', 'now', 'localtime'), 'roll', null, null);
+
 
 -- create table reply (
 --     reply_id integer primary key,
 --     reply_post_id integer,
 --     user text not null,
 --     date text not null,
---     post text not null,
---     image_file text,
+--     reply text,
+--     img_filename text,
+--     img_uniqid text,
 --     foreign key(reply_post_id) references post(post_id)
 -- );
--- server.py
+++ server.py
@@ -1,20 +1,22 @@
 import datetime
 import os
 import random
+import re
 import sqlite3
 from typing import NamedTuple
+from time import time
 
-from flask import Flask, g, render_template, request
+from flask import Flask, g, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
 
+app = Flask(__name__, static_url_path='/static')
+
+UPLOAD_FOLDER = 'UPLOAD_FOLDER'
+app.config[UPLOAD_FOLDER] = 'static/img_uploads'
+
 DATABASE = 'db/img_board.db'
 SCHEMA = 'db/schema.sql'
-UPLOAD_FOLDER = 'static/uploads'
-IMG_EXTS = set(['png', 'jpg', 'jpeg'])
-
-app = Flask(__name__, static_url_path='/static')
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 # Dot notation > dictionary syntax for use in our HTML
@@ -29,22 +31,26 @@ class Post(NamedTuple):
     user: str
     date: str
     post: str
-    image_file: str
+    image_filename: str
+    image_uniqid: str
+    image_ext: str
 
 class Reply(NamedTuple):
     reply_id: int
     reply_post_id: int
     user: str
     date: str
-    post: str
-    image_file: str
+    reply: str
+    image_filename: str
+    image_uniqid: str
+    image_ext: str
 
 
 @app.route('/')
 def home():
     sql_string = 'select * from board'
     boards = query_db(sql_string)
-    board_NTs = [Board(board[0], board[1], board[2]) for board in boards]
+    board_NTs = [Board(*board) for board in boards]
     return render_template('home.html', boards=board_NTs)
 
 
@@ -52,39 +58,120 @@ def home():
 def board(board_acronym):
     sql_string = 'select post.* from board, post where board_acronym=? and post_board_id=board_id'
     posts = query_db(sql_string, args=[board_acronym])
-    if posts:
-        post_NTs = [Post(post[0], post[1], post[2], post[3], post[4], post[5]) for post in posts]
-        return render_template('board.html', board_acronym=board_acronym, posts=post_NTs)
-    else:
-        return home()
+    # if posts:
+    post_NTs = []
+    for post in posts:
+        # Want to make use of argument unpacking, Post(*post_cpy)
+        # Need to unlock the tuple to change some args
+        post_cpy = list(post)
+        post_cpy[6] = str(post[6])
+        post_cpy.append(get_file_ext(post[5]))
+        nt = Post(*post_cpy)
+        post_NTs.append(nt)
+    return render_template('board.html', board_acronym=board_acronym, posts=post_NTs)
+    # else:
+    #     return redirect(url_for('home'))
 
 
 @app.route('/<board_acronym>/post', methods=['POST'])
 def post(board_acronym):
     sql_string = 'select board_id from board, post where board_acronym=? and post_board_id=board_id'
-    board_id = query_db(sql_string, args=[board_acronym], one=True)[0]
-    image_file = 'test_img1'
+    try:
+        board_id = query_db(sql_string, args=[board_acronym], one=True)[0]
+    except:
+        enum = {'g': 1, 'ck': 2, 'b': 3}
+        board_id = enum[board_acronym]
+
+    post = request.form.get('post_upload_text')
+    
+    image_uniqid = None
+    image_obj = request.files['post_upload_image']
+    image_obj.filename = image_obj.filename.lower()
+    if image_obj.filename != '':
+        image_uniqid = get_new_uniqid()
+        upload_successful = upload_img(image_obj, image_uniqid)
+        if not upload_successful:
+            print('Upload unsuccessful.')
+            return redirect(url_for('board', board_acronym=board_acronym))
+
     if not board_id or type(board_id)!=int:
-        print(f'board_id: {board_id}')
-        raise ValueError
-    post_rows = create_post(board_id, request.form.get('post_text'), image_file)
-    print(f'Post upload successfull. {post_rows} rows.')
-    return board(board_acronym)
+        raise ValueError(f'board_id: {board_id}')
+
+    # Important check since we do not enforce NOT NULL in the db schema
+    if post or image_obj.filename:
+        create_post(board_id, post, image_obj.filename, image_uniqid)
+    
+    print_upload_task(post, image_obj.filename)
+
+    return redirect(url_for('board', board_acronym=board_acronym))
 
 
-def create_post(post_board_id, post, image_file):
+
+def print_upload_task(post, filename):
+    msg = 'Uploaded: '
+    if post:
+        msg+='post'
+    if filename:
+        msg+='image'
+    if not post and not filename:
+        msg+='nothing'
+    msg+='.'
+    print(msg)
+
+
+def create_post(post_board_id, post, image_filename, image_uniqid):
     try:
         user = 'anon' + str(random.randint(10_000, 99_999))
-        sql_string = """insert into post(post_board_id, user, date, post, image_file)
-                            values (?, ?, strftime('%Y-%m-%d %H:%M', 'now', 'localtime'), ?, ?);"""
+        sql_string = """insert into post(post_board_id, user, date, post, image_filename, image_uniqid)
+                            values (?, ?, strftime('%Y-%m-%d %H:%M', 'now', 'localtime'), ?, ?, ?);"""
         db = get_db()
         cur = db.cursor()
-        cur.execute(sql_string, [post_board_id, user, post, image_file])
+        if image_filename == '': image_filename = None
+        if post == '': post = None
+        cur.execute(sql_string, [post_board_id, user, post, image_filename, image_uniqid])
         db.commit()
         cur.close()
         return cur.lastrowid
     except Exception as e:
         raise ValueError(e)
+
+
+def get_new_uniqid():
+    # e.g. 0x385a482f97b356[2:]
+    image_uniqid = hex(int(time()*10_000_000))[2:]
+    return image_uniqid
+
+
+def get_file_ext(filename):
+    if filename:
+        return '.' + filename.split('.')[-1].lower()
+    else:
+        return str(filename)
+
+
+def upload_img(image_obj, image_uniqid):
+    """https://www.reddit.com/r/learnprogramming/comments/gamhq/generating_a_short_unique_id_in_python_similar_to/
+    """
+    if image_obj and file_allowed(image_obj.filename):
+        # >>> secure_filename('file!@#$%^&*.123_name1234..png')
+        # 'file.123_name1234..png'
+        filename = secure_filename(image_obj.filename)
+        # .png
+        extension = get_file_ext(filename)
+        # 385a482f97b356.png
+        image_stored_filename = image_uniqid + extension
+        # db/img_uploads/385a482f97b356.png
+        image_path = os.path.join(app.config[UPLOAD_FOLDER], image_stored_filename)
+        image_obj.save(image_path)
+        return True
+    else:
+        return False
+
+
+def file_allowed(filename):
+    # <something> then .png, .jpg, or .jpeg
+    match = bool(re.match(r'^.+\.(png|jpg|jpeg|gif)$', filename))
+    return match
 
 
 def init_db():
